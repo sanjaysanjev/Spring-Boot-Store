f56d8b077e54d9ab70bd9d0c1d9abf55490c9741
sanjaysanjev <s.sanjay.eda@gmail.com>
2026-08-22 15:44:43 +0530

    checking

--- src/main/java/com/sanjay/store/StoreApplication.java
+++ src/main/java/com/sanjay/store/StoreApplication.java
@@ -8,14 +8,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        /*Dotenv dotenv = Dotenv.load();
 
-
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-
-        System.setProperty("STRIPE_SECRET_KEY", dotenv.get("STRIPE_SECRET_KEY"));
-
-        System.setProperty("STRIPE_WEBHOOK_SECRET_KEY",dotenv.get("STRIPE_WEBHOOK_SECRET_KEY"));*/
 
         SpringApplication.run(StoreApplication.class, args);
     }
